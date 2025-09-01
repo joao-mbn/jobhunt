e6009f6932881e3501551be687065841161bc132
João Nascimento <joaombn97@gmail.com>
2025-09-01 10:42:08 -0300

    correcting path for resume

--- src/file-system/file-system.ts
+++ src/file-system/file-system.ts
@@ -3,7 +3,7 @@ import type { ResumeData } from "../types/types.ts";
 
 export function loadResumeData(): ResumeData {
   try {
-    const resumePath = "my-resume.json";
+    const resumePath = "data/my-resume.json";
     const resumeContent = fs.readFileSync(resumePath, "utf-8");
     return JSON.parse(resumeContent);
   } catch (error) {

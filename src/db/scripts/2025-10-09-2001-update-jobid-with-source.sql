c5b807627cfad69fc99984191fcb7197e89d21ba
João Nascimento <joaombn97@gmail.com>
2025-10-09 20:43:10 -0700

    Update the job_id in the raw_jobs, clean_jobs, and enhanced_jobs and prefills tables   to include the source of the job for improved readability.

--- src/db/scripts/2025-10-09-2001-update-jobid-with-source.sql
+++ src/db/scripts/2025-10-09-2001-update-jobid-with-source.sql
@@ -1,3 +1,8 @@
+/*
+  Update the job_id in the raw_jobs, clean_jobs, and enhanced_jobs and prefills tables
+  to include the source of the job for improved readability.
+ */
+
 PRAGMA foreign_keys = OFF;
 
 UPDATE raw_jobs
@@ -8,13 +13,10 @@ UPDATE clean_jobs
 SET job_id = "source"||'-'||job_id
 WHERE "source" in ('linkedin', 'levels');
 
-UPDATE prefills
-SET enhanced_job_id = "source"||'-'||enhanced_job_id
-WHERE enhanced_job_id IN (
-    SELECT job_id
-    FROM enhanced_jobs
-    WHERE "source" in ('linkedin', 'levels')
-);
+UPDATE prefills as p
+SET enhanced_job_id = ej."source"||'-'||enhanced_job_id
+FROM enhanced_jobs ej
+WHERE ej."source" in ('linkedin', 'levels') and p.enhanced_job_id = ej.job_id;
 
 UPDATE enhanced_jobs
 SET job_id = "source"||'-'||job_id
